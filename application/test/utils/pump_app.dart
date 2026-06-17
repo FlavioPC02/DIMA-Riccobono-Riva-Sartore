42a037e7211a8a928f31029f2b89b64fbdc52f73
FlavioPC02 <flavio.riccobono@mail.polimi.it>
2026-06-17 20:04:55 +0200

    fixig

--- application/test/utils/pump_app.dart
+++ application/test/utils/pump_app.dart
@@ -1,14 +1,8 @@
 import 'package:application/core/cubit/activity_cubit.dart';
 import 'package:application/core/cubit/profile_cubit.dart';
 import 'package:application/core/cubit/settings_cubit.dart';
-<<<<<<< HEAD
-import 'package:application/core/repository/activity_repository.dart';
-=======
-import 'package:application/core/models/activity.dart';
->>>>>>> activity
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mocktail/mocktail.dart';
 
 import '../mocks/mocks_manual.dart';
 
@@ -18,39 +12,17 @@ Widget pumpApp({
   ProfileCubit? profileCubit,
   ActivityCubit? activityCubit,
 }) {
-  final providedActivityCubit = activityCubit;
-  final resolvedActivityCubit = providedActivityCubit ?? MockActivityCubit();
-  if (providedActivityCubit == null) {
-    when(
-      () => resolvedActivityCubit.stream,
-    ).thenAnswer((_) => const Stream<List<Activity>>.empty());
-    when(() => resolvedActivityCubit.state).thenReturn(const <Activity>[]);
-  }
-
   return MultiBlocProvider(
     providers: [
-<<<<<<< HEAD
       BlocProvider<SettingsCubit>.value(value: settingsCubit ?? MockSettingsCubit(),),
       BlocProvider<ProfileCubit>.value(value: profileCubit ?? MockProfileCubit(),),
-      BlocProvider<ActivityCubit>.value(
-        value: activityCubit ?? ActivityCubit(
-          ActivityRepository(hasCurrentUser: () => false),
-        ),
-      ),
+      BlocProvider<ActivityCubit>.value(value: activityCubit ?? MockActivityCubit(),)
     ],
     child: MaterialApp(
       theme: ThemeData(
         useMaterial3: false,
-=======
-      BlocProvider<SettingsCubit>.value(
-        value: settingsCubit ?? MockSettingsCubit(),
->>>>>>> activity
       ),
-      BlocProvider<ProfileCubit>.value(
-        value: profileCubit ?? MockProfileCubit(),
-      ),
-      BlocProvider<ActivityCubit>.value(value: resolvedActivityCubit),
-    ],
-    child: MaterialApp(theme: ThemeData(useMaterial3: false), home: child),
+      home: child,
+    ),
   );
 }
